537fcc1ee903155ef715e97eab470a3e8ac5615b
rhaprace <rhaprace@gmail.com>
2025-06-26 15:22:14 +0800

    minimal update

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import path from 'path'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+import { name } from './package.json'
+const base = process.env.NODE_ENV === 'production' ? `/${name}/` : '/'
+
 export default defineConfig({
+  base,
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
@@ -12,6 +17,18 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'ui-vendor': ['lucide-react']
+        }
+      }
     }
   }
 })
